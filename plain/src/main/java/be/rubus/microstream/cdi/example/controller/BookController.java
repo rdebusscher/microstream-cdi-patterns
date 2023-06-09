f3fab2fc12170c06d8a86e1c84290d15a9edeabd
Rudy De Busscher <rdebusscher@gmail.com>
2023-06-09 13:31:52 +0200

    Add Book By ISBN endpoint and update to version 7.1 final

--- plain/src/main/java/be/rubus/microstream/cdi/example/controller/BookController.java
+++ plain/src/main/java/be/rubus/microstream/cdi/example/controller/BookController.java
@@ -7,9 +7,12 @@ import be.rubus.microstream.cdi.example.service.BookService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
+import java.util.Optional;
 
 @Path("/book")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,5 +27,15 @@ public class BookController {
         return bookService.getAll();
     }
 
+    @GET()
+    @Path("{isbn}")
+    public Response getByIsbn(@PathParam("isbn") String isbn) {
+        Optional<Book> byISBN = bookService.getBookByISBN(isbn);
+        if (byISBN.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.ok().entity(byISBN.get()).build();
+        }
+    }
 
 }
